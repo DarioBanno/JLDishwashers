efb1479fcacdb82df9ba49d4f64783f63b6e0dd9
Dario <sonixant@gmail.com>
2017-02-14 10:01:24 +0000

    Implemented empty Product model + Unit Tests (currently not passing)

---
+++ JLDishwashers/JLDishwashers/Product.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  JLDishwashers
+//
+//  Created by Dario Banno on 14/02/2017.
+//  Copyright © 2017 AppTown. All rights reserved.
+//
+
+import Foundation
+
+struct Product: JSONParsable {
+
+    init?(json: JSONObject?) {
+        // TODO
+    }
+}
---
+++ JLDishwashers/JLDishwashersTests/ProductTests.swift
@@ -0,0 +1,54 @@
+//
+//  ProductTests.swift
+//  JLDishwashers
+//
+//  Created by Dario Banno on 14/02/2017.
+//  Copyright © 2017 AppTown. All rights reserved.
+//
+
+import XCTest
+@testable import JLDishwashers
+
+class ProductTests: XCTestCase {
+    
+    var product: Product!
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func test_product_is_created_from_JSON() {
+        
+        // GIVEN I have a JSON Object for product
+        let jsonStub = JSONStubber.load(file: "Product")
+        
+        // WHEN I create a Product from json
+        let product = Product(json: jsonStub)
+        
+        // THEN the product should not be nil
+        XCTAssertNotNil(product)
+        
+        // AND should contain values from the JSON object
+//        XCTAssertEqual(product?.productId, "1913470")
+//        XCTAssertEqual(product?.image, "//johnlewis.scene7.com/is/image/JohnLewis/234326372?")
+//        XCTAssertEqual(product?.title, "Bosch SMV53M40GB Fully Integrated Dishwasher")
+//        XCTAssertEqual(product?.outOfStock, false)
+    }
+    
+    func test_product_is_not_created_when_JSON_is_incomplete() {
+        
+        // GIVEN I have a JSON Object for product
+        let jsonStub = JSONStubber.load(file: "Product_incomplete")
+        
+        // WHEN I create a Product from json
+        let product = Product(json: jsonStub)
+        
+        // THEN the product should not be nil
+        XCTAssertNil(product)
+    }
+
+}
