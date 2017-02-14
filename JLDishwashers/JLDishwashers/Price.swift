5fc18a14514e7690444ce3e5e4e17aa443f58189
Dario <sonixant@gmail.com>
2017-02-14 10:42:05 +0000

    Implemented empty Price model + Unit Tests (currently not passing)

---
+++ JLDishwashers/JLDishwashers/Price.swift
@@ -0,0 +1,15 @@
+//
+//  Price.swift
+//  JLDishwashers
+//
+//  Created by Dario Banno on 14/02/2017.
+//  Copyright © 2017 AppTown. All rights reserved.
+//
+
+import Foundation
+
+struct Price: JSONParsable {
+    init?(json: JSONObject?) {
+        // TODO
+    }
+}
