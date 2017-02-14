c21af84650720dd104945a828d0b69aeb04d3420
Dario <sonixant@gmail.com>
2017-02-14 10:00:33 +0000

    Created JSONStubber + Product JSON stubs for Unit Tests

---
+++ JLDishwashers/JLDishwashersTests/JSONStubber.swift
@@ -0,0 +1,22 @@
+//
+//  JSONStubber.swift
+//  JLDishwashers
+//
+//  Created by Dario Banno on 14/02/2017.
+//  Copyright © 2017 AppTown. All rights reserved.
+//
+
+import Foundation
+@testable import JLDishwashers
+
+class JSONStubber {
+    static func load(file: String) -> JSONObject? {
+        guard let path = Bundle(for: type(of: JSONStubber())).path(forResource: file, ofType: "json"),
+            let data = try? Data(contentsOf: URL(fileURLWithPath: path)),
+            let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? JSONObject else {
+                debugPrint("Unable to load JSON from file: \(file)")
+                return nil
+        }
+        return json
+    }
+}
