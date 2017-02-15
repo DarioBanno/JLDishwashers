71b3baf9092572fa15cc668abe93bb6242881d2f
Dario <sonixant@gmail.com>
2017-02-15 20:20:37 +0000

    Image Slider: implemented slides and image loading

--- JLDishwashers/JLDishwashers/ImageSliderViewController.swift
+++ JLDishwashers/JLDishwashers/ImageSliderViewController.swift
@@ -13,8 +13,37 @@ class ImageSliderViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.isPagingEnabled = true
+    }
+    
     func configure(with urls: [URL]) {
-        // TODO
+        // remove all previous views
+        scrollView.subviews.forEach { $0.removeFromSuperview() }
+        
+        // set content size
+        var contentSize = scrollView.bounds.size
+        contentSize.width *= CGFloat(urls.count)
+        scrollView.contentSize = contentSize
+        
+        // udate page control
+        pageControl.numberOfPages = urls.count
+        
+        // create slides
+        for index in 0..<urls.count {
+            let imageView = generateImageSlide(at: index)
+            imageView.backgroundColor = .red
+            imageView.load(from: urls[index])
+            scrollView.addSubview(imageView)
+        }
+    }
+
+    func generateImageSlide(at index: Int) -> URLImageView {
+        var frame = scrollView.bounds
+        frame.origin.x = frame.width * CGFloat(index)
+        return URLImageView(frame: frame)
     }
     
 }
+
