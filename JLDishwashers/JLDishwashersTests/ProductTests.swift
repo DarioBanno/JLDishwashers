a2226dfab1139d28c2d64c441db4022fa7671c1b
Dario <sonixant@gmail.com>
2017-02-15 12:00:40 +0000

    Updating tests for ProductTests to use product from ProductList_search. Removing Product.json

--- JLDishwashers/JLDishwashersTests/ProductTests.swift
+++ JLDishwashers/JLDishwashersTests/ProductTests.swift
@@ -21,31 +21,33 @@ class ProductTests: XCTestCase {
         super.tearDown()
     }
     
-    func test_product_is_created_from_JSON() {
+    func test_product_is_created_from_JSON_ProductList_Search() {
         
         // GIVEN I have a JSON Object for product
-        let jsonStub = JSONStubber.load(file: "Product")
+        let searchJson = JSONStubber.load(file: "ProductList_search")!
+        let productListJson = searchJson["products"] as! [JSONObject]
+        let productJson = productListJson[0]
         
         // WHEN I create a Product from json
-        let product = Product(json: jsonStub)
+        let product = Product(json: productJson)
         
         // THEN the product should not be nil
         XCTAssertNotNil(product)
         
         // AND should contain values from the JSON object
-        XCTAssertEqual(product?.productId, "1913470")
+        XCTAssertEqual(product?.productId, "001")
         XCTAssertEqual(product?.image, "//johnlewis.scene7.com/is/image/JohnLewis/234326372?")
-        XCTAssertEqual(product?.title, "Bosch SMV53M40GB Fully Integrated Dishwasher")
+        XCTAssertEqual(product?.title, "Donald Trump Fully Integrated Dishwasher")
         XCTAssertEqual(product?.outOfStock, false)
         XCTAssertNotNil(product?.price)
-        XCTAssertEqual(product?.price?.now, "499.00")
-        XCTAssertEqual(product?.price?.currency, "GBP")
+        XCTAssertEqual(product?.price?.now, "10,000,000.00")
+        XCTAssertEqual(product?.price?.currency, "USD")
     }
     
     func test_product_is_not_created_when_JSON_is_incomplete() {
         
         // GIVEN I have a JSON Object for product
-        let jsonStub = JSONStubber.load(file: "Product_incomplete")
+        let jsonStub = JSONStubber.load(file: "Product_no_productId")!
         
         // WHEN I create a Product from json
         let product = Product(json: jsonStub)
