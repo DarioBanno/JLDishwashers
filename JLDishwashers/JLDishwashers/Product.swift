44e308b2cecb5a24c8bc970ee9de90d99ba6e9bf
Dario <sonixant@gmail.com>
2017-02-15 13:32:01 +0000

    Implemented Product Media and Images population

--- JLDishwashers/JLDishwashers/Product.swift
+++ JLDishwashers/JLDishwashers/Product.swift
@@ -15,6 +15,20 @@ struct Product: JSONParsable {
     var image: String?
     var outOfStock: Bool?
     var price: Price?
+    var media: Media?
+    
+    struct Media: JSONParsable {
+        var images: Images?
+        init?(json: JSONObject?) {
+            images = Images(json: json?["images"] as? JSONObject)
+        }
+    }
+    struct Images: JSONParsable {
+        var urls: [String]?
+        init?(json: JSONObject?) {
+            urls = json?["urls"] as? [String]
+        }
+    }
     
     init?(json: JSONObject?) {
         productId = json?["productId"] as? String
@@ -22,6 +36,7 @@ struct Product: JSONParsable {
         image = json?["image"] as? String
         outOfStock = json?["outOfStock"] as? Bool
         price = Price(json: json?["price"] as? JSONObject)
+        media = Media(json: json?["media"] as? JSONObject)
         
         // Validate model
         guard productId != nil else {
