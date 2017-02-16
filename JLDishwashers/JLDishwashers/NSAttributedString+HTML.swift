d01d0a0bec5ebe0cd21b5fdf2bc98520371926fc
Dario <sonixant@gmail.com>
2017-02-16 23:18:58 +0000

    Documentation for NSAttributedString+HTML

--- JLDishwashers/JLDishwashers/NSAttributedString+HTML.swift
+++ JLDishwashers/JLDishwashers/NSAttributedString+HTML.swift
@@ -10,8 +10,19 @@ import UIKit
 
 extension NSAttributedString {
 
+    /// Initialize an NSAttributedString from an HTML string.
+    ///
+    /// Parameters:
+    /// - html: the HTML string
+    /// - usingFont: (optional) the font the should be used if no font is provided in the html
+    ///
+    /// Please note:
+    /// - if font contains any bold or italic attributes, these will override any font attribute specified in HTML
+    /// - NSAttributedString will default to Times New Roman if no font is provided in both html or usingFont
+    ///
     convenience init?(html: String, usingFont font: UIFont? = nil) {
-        
+       
+        // By default NSAttributedString uses TimesNewRoman for HTML, therefore we need to manually apply a div containing a global font style.
         var htmlString = html
         if let font = font {
             let fontStyleTag = "style=\"font-family: '\(font.fontName)'; font-size: \(font.pointSize)\""
@@ -22,7 +33,7 @@ extension NSAttributedString {
             return nil
         }
         
-        var options: [String: Any] = [
+        let options: [String: Any] = [
             NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
             NSCharacterEncodingDocumentAttribute: NSNumber(value: String.Encoding.utf8.rawValue),
         ]
