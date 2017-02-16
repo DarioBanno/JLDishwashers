6f1707aa81d4c0a99f3ec2e409a18211c47f7a21
Dario <sonixant@gmail.com>
2017-02-16 21:41:03 +0000

    Added font parameter to NSAttributedString+HTML initializer + Unit Tests ✅

--- JLDishwashers/JLDishwashers/NSAttributedString+HTML.swift
+++ JLDishwashers/JLDishwashers/NSAttributedString+HTML.swift
@@ -10,15 +10,25 @@ import UIKit
 
 extension NSAttributedString {
 
-    convenience init?(html: String) {
-        guard let data = html.data(using: String.Encoding.utf8, allowLossyConversion: true) else {
+    convenience init?(html: String, usingFont font: UIFont? = nil) {
+        
+        var htmlString = html
+        if let font = font {
+            let fontStyleTag = "style=\"font-family: '\(font.fontName)'; font-size: \(font.pointSize)\""
+            htmlString = "<div \(fontStyleTag)>\(html)</div>"
+        }
+        
+        guard let data = htmlString.data(using: String.Encoding.utf8, allowLossyConversion: true) else {
             return nil
         }
+        
+        var options: [String: Any] = [
+            NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
+            NSCharacterEncodingDocumentAttribute: NSNumber(value: String.Encoding.utf8.rawValue),
+        ]
+        
         do {
-            try self.init(data: data, options: [
-                NSDocumentTypeDocumentAttribute: NSHTMLTextDocumentType,
-                NSCharacterEncodingDocumentAttribute: NSNumber(value: String.Encoding.utf8.rawValue)
-                ], documentAttributes: nil)
+            try self.init(data: data, options: options, documentAttributes: nil)
         } catch {
             return nil
         }
--- JLDishwashers/JLDishwashersTests/NSAttributedString+HTMLTests.swift
+++ JLDishwashers/JLDishwashersTests/NSAttributedString+HTMLTests.swift
@@ -33,4 +33,29 @@ class NSAttributedString_HTMLTests: XCTestCase {
         XCTAssertEqual(attributedString?.string, "\n\nLOREM IPSUM\nQUE DOLOR!\n")
     }
     
+    func test_attributed_string_is_initialized_from_html_with_font() {
+        // GIVEN I have an HTML String
+        let htmlString = "<br><br>LOREM IPSUM<br>QUE DOLOR!<br>"
+        
+        // AND a given font
+        let font = UIFont(name: "Helvetica Neue", size: 13)!
+        
+        // WHEN I create an attributed string
+        let attributedString = NSAttributedString(html: htmlString, usingFont: font)
+        
+        // THEN attributedString should not be nil
+        XCTAssertNotNil(attributedString)
+        
+        // AND attributedString should equal the original html string
+        XCTAssertEqual(attributedString?.string, "\n\nLOREM IPSUM\nQUE DOLOR!\n")
+        
+        // AND font attribute should match the one provided
+        var attributedStringFont: UIFont!
+        let attributedStringRange: NSRange = NSMakeRange(attributedString!.string.characters.distance(from: attributedString!.string.startIndex, to: attributedString!.string.startIndex), attributedString!.string.characters.distance(from: attributedString!.string.startIndex, to: attributedString!.string.endIndex))
+        attributedString!.enumerateAttribute(NSFontAttributeName, in: attributedStringRange, options: NSAttributedString.EnumerationOptions(rawValue: 0)) { (value, range, stop) in
+            attributedStringFont = value as? UIFont
+        }
+        XCTAssertEqual(attributedStringFont, font)
+    }
+    
 }
