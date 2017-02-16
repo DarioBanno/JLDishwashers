833ad6babbf01870640ecfd233fd2bec53f569d4
Dario <sonixant@gmail.com>
2017-02-16 23:18:33 +0000

    Implemented AttributeItemView

---
+++ JLDishwashers/JLDishwashers/AttributeItemView.swift
@@ -0,0 +1,21 @@
+//
+//  AttributeItemView.swift
+//  JLDishwashers
+//
+//  Created by Dario Banno on 16/02/2017.
+//  Copyright © 2017 AppTown. All rights reserved.
+//
+
+import UIKit
+
+class AttributeItemView: UIView {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var valueLabel: UILabel!
+    
+    override func awakeFromNib() {
+        nameLabel.font = Resource.Font.mediumRegular
+        valueLabel.font = Resource.Font.mediumRegular
+    }
+    
+}
